bea8f0d4b535ce3a66b82e822037b910843c808e
juricdu <juricdu@SP08198.wuh-intern.de>
2017-12-15 16:12:50 +0100

    Some works on GUI

--- __main__.py
+++ __main__.py
@@ -5,6 +5,8 @@ Created on Dec 15, 2017
 '''
 
 from tkinter import *
+from sources.functions import *
+
 
 mainWindow = Tk()
 
@@ -12,4 +14,24 @@ mainWindow.title("Configuration thingy")
 
 if __name__ == '__main__':
     print ("debug info {}".format(__name__))
+    
+    btn01 = Button(mainWindow, text='sample text', padx=3, pady=2)
+    
+    menuStrip = Menu(mainWindow)
+    fileMenu = Menu(menuStrip, tearoff=0)
+    editMenu = Menu(menuStrip, tearoff=0)
+    
+    fileMenu.add_command(label="Don't do a thing")
+    fileMenu.add_command(label="Exit", command=mainWindow.quit)
+    
+    editMenu.add_command(label="Publish configuration", command=commPublish)
+    editMenu.add_separator()
+    editMenu.add_command(label="Repair configuration", command=commRepair)
+    
+    menuStrip.add_cascade(label="File", menu=fileMenu)
+    menuStrip.add_cascade(label="Operations", menu=editMenu)
+    
+    btn01.grid(row=0, column=0)
+    mainWindow.geometry("800x600")
+    mainWindow.config(menu=menuStrip)
     mainWindow.mainloop()
---
+++ sources/functions.py
@@ -0,0 +1,11 @@
+def setupGUI():
+    pass
+
+
+# GUI commands
+
+def commPublish():
+    print("Publish function via menuCommand")
+
+def commRepair():
+    print("Repair function via menuCommand")
