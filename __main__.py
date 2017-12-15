013c1c748badcd7f0d3013d858c2b94334a16085
Dušan Jurica <dusan.jurica@wuh-group.com>
2017-12-15 13:20:10 +0100

    Initial commit

---
+++ __main__.py
@@ -0,0 +1,15 @@
+'''
+Created on Dec 15, 2017
+
+@author: juricdu
+'''
+
+from tkinter import *
+
+mainWindow = Tk()
+
+mainWindow.title("Configuration thingy")
+
+if __name__ == '__main__':
+    print ("debug info {}".format(__name__))
+    mainWindow.mainloop()
